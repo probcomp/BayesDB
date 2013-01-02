c7b9738f6e6ecc2695dee43a6785cf50a907052a
dlovell <dlovell@gmail.com>
2013-01-02 12:30:00 -0500

    remove functions that operate on objects from numerics

--- test_cluster.cpp
+++ test_cluster.cpp
@@ -81,9 +81,12 @@ int main(int argc, char** argv) {
   std::cout << "show use of underlying numerics functions" << std::endl;
   std::cout << "calc_continuous_logp(0, 1, 2, 2, 0)" << std::endl;
   std::cout << numerics::calc_continuous_logp(0, 1, 2, 2, 0) << std::endl;
-
   std::cout << "calc_cluster_crp_logp(10, 100, 10)" << std::endl;
   std::cout << numerics::calc_cluster_crp_logp(10, 100, 10) << std::endl;
+
   
+  // std::cout << "numerics::calc_cluster_vector_joint_logp(cd, V)" << std::endl;
+  // std::cout << numerics::calc_cluster_vector_joint_logp(cd, V) << std::endl;
+
   std::cout << std::endl << "Goodbye World!" << std::endl;
 }
