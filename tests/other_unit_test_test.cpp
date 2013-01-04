46107a6749b4bfa3b3b0c9cc3c635ec57ae9619d
dlovell <dlovell@gmail.com>
2013-01-04 14:16:35 -0500

    remove test stubs

--- tests/other_unit_test_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-int main(int argc, char** argv) {
-  cout << "Hello World!" << endl;
-  cout << "I'm a ALSO unit test test!" << endl;
-  cout << "Goodbye World!" << endl;
-}
