cf3cd6811b3abc898efdba7ef380c7ce87f9db45
dlovell <dlovell@gmail.com>
2013-01-14 15:39:56 -0500

    transition views individually incremental progress towards State::transition

--- tests/test_state.cpp
+++ tests/test_state.cpp
@@ -16,6 +16,8 @@ int main(int argc, char** argv) {
   LoadData("Synthetic_data.csv", data);
   int num_rows = data.size1();
   int num_cols = data.size2();
+  //
+  map<int, vector<double> > data_map = construct_data_map(data);
 
   // create the objects to test
   vector<int> global_row_indices = create_sequence(data.size1());
@@ -29,5 +31,25 @@ int main(int argc, char** argv) {
     cout << "view idx " << view_idx << ": ";
     v.print();
   }
+
+  for(int view_idx=0; view_idx<num_views; view_idx++) {
+    cout << "transitioning view: " << view_idx << endl;
+    View &v = s.get_view(view_idx);
+    vector<int> view_cols = get_indices_to_reorder(global_column_indices, v.global_to_local);
+    MatrixD data_subset = extract_columns(data, view_cols);
+    cout << "data_subset: " << data_subset << endl;
+    map<int, vector<double> > data_subset_map = construct_data_map(data_subset);
+    for(int i=0;i<4;i++) {
+      cout << "transition #: " << i << endl;
+      v.transition(data_subset_map);
+    }
+  }
+
+  for(int view_idx=0; view_idx<num_views; view_idx++) {
+    View &v = s.get_view(view_idx);
+    cout << "view_idx " << view_idx << ": " << endl;
+    v.print();
+  }
+
   cout << endl << "Goodbye World!" << endl;
 }
