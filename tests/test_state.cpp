5d4a50b869d562edaf5b91109941e356cb159319
dlovell <dlovell@gmail.com>
2013-01-14 13:23:39 -0500

    add test of state object

---
+++ tests/test_state.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "State.h"
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+typedef boost::numeric::ublas::matrix<double> matrixD;
+using namespace std;
+
+int main(int argc, char** argv) {
+  cout << endl << "Hello World!" << endl;
+
+  // load some data
+  matrixD data;
+  LoadData("Synthetic_data.csv", data);
+  int num_rows = data.size1();
+  int num_cols = data.size2();
+
+  // create the objects to test
+  vector<int> global_row_indices = create_sequence(data.size1());
+  vector<int> global_column_indices = create_sequence(data.size2());
+  State s = State(data, global_row_indices, global_column_indices, 11);
+
+  int num_views = s.get_num_views();
+  cout << "s.num_views: " << num_views << endl;
+  for(int view_idx=0; view_idx<num_views; view_idx++) {
+    View &v = s.get_view(view_idx);
+    cout << "view idx " << view_idx << ": ";
+    v.print();
+  }
+  cout << endl << "Goodbye World!" << endl;
+}
