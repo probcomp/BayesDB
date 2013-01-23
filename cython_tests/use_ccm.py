f5c9884e8702523a385d15d3be4fef9b088f2072
dlovell <dlovell@gmail.com>
2013-01-22 23:25:26 -0500

    add files to demonstrate continuous component model demonstration

---
+++ cython_tests/use_ccm.py
@@ -0,0 +1,15 @@
+import continuous_component_model_test as ccmt
+
+hyper_map = dict()
+hyper_map["mu"] = 1.0
+hyper_map["s"] = 1.0
+hyper_map["nu"] = 1.0
+hyper_map["r"] = 1.0
+
+ccmt.set_string_double_map(hyper_map)
+component_model = ccmt.p_ContinuousComponentModel(hyper_map)
+print component_model.calc_marginal_logp()
+component_model.insert_element(1.0)
+print component_model.calc_marginal_logp()
+component_model.remove_element(1.0)
+print component_model.calc_marginal_logp()
