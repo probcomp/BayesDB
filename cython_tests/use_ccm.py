6263686f09e939485d38d2d7ce597fe9bb6837a4
dlovell <dlovell@gmail.com>
2013-01-27 13:14:06 -0500

    test State

--- cython_tests/use_ccm.py
+++ cython_tests/use_ccm.py
@@ -1,5 +1,6 @@
 import continuous_component_model_test as ccmt
 import boost_matrix_test as bmt
+import State
 
 hyper_map = dict()
 hyper_map["mu"] = 1.0
@@ -15,9 +16,16 @@ print component_model.calc_marginal_logp()
 component_model.remove_element(1.0)
 print component_model.calc_marginal_logp()
 
-p_matrix = bmt.p_matrix(5,5)
+num_rows = 5
+num_cols = 4
+p_matrix = bmt.p_matrix(num_rows, num_cols)
 print p_matrix
 print "p_matrix.get(0, 0):", p_matrix.get(0, 0)
 p_matrix.set(0,0, -1.)
 print "p_matrix.get(0, 0):", p_matrix.get(0, 0)
 
+global_row_indices = range(num_rows)
+global_col_indices = range(num_cols)
+import numpy
+my_array = numpy.ndarray((num_rows, num_cols))
+p_State = State.p_State(my_array, global_row_indices, global_col_indices, 31, 0)
