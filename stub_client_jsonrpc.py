d4c863161757f010c76658d6493d3e96c8e84fbf
dlovell <dlovell@gmail.com>
2013-01-30 14:29:23 -0500

    call analyze

--- stub_client_jsonrpc.py
+++ stub_client_jsonrpc.py
@@ -29,31 +29,53 @@ def call_and_print(method_name, args_dict):
     print 'trying message:', message
     r = requests.put(URI, data=message)
     r.raise_for_status()
-    print json.loads(r.content)
+    out = json.loads(r.content)
+    print out
     print
+    if isinstance(out, dict) and 'result' in out:
+        return out['result']
+    else:
+        # error?
+        return out
+
+T_dim = (3, 4)
+T = (numpy.arange(numpy.prod(T_dim)).reshape(T_dim) * 1.0).tolist()
 
 # non-stub functions
-non_stub = set(['initialize', 'initialize_and_analyze'])
+non_stub = set(['initialize', 'initialize_and_analyze', 'analyze'])
 #
 method_name = 'initialize'
 args_dict = dict()
 args_dict['M_c'] = 'M_c'
 args_dict['M_r'] = 'M_r'
 args_dict['i'] = ''
-T_dim = (3, 4)
-T = (numpy.arange(numpy.prod(T_dim)).reshape(T_dim) * 1.0).tolist()
 args_dict['T'] = T
-call_and_print(method_name, args_dict)
+out = call_and_print(method_name, args_dict)
 time.sleep(1)
 #
 method_name = 'initialize_and_analyze'
 args_dict = dict()
 args_dict['n_steps'] = 10
 args_dict['SEED'] = 0
-T_dim = (3, 4)
-T = (numpy.arange(numpy.prod(T_dim)).reshape(T_dim) * 1.0).tolist()
 args_dict['T'] = T
-call_and_print(method_name, args_dict)
+out = call_and_print(method_name, args_dict)
+time.sleep(1)
+
+
+X_L_prime, X_D_prime = out
+method_name = 'analyze'
+args_dict = dict()
+args_dict['M_c'] = 'M_c'
+args_dict['T'] = T
+args_dict['X_L'] = X_L_prime
+args_dict['X_D'] = X_D_prime
+args_dict['kernel_list'] = 'kernel_list'
+args_dict['n_steps'] = 2
+args_dict['c'] = 'c'
+args_dict['r'] = 'r'
+args_dict['max_iterations'] = 'max_iterations'
+args_dict['max_time'] = 'max_time'
+out = call_and_print(method_name, args_dict)
 time.sleep(1)
 
 # programmatically call all the other method calls
