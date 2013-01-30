a4c1f872f8348ccb299d359d4ffdcd528b4721ff
Peter Krafft <peter@unnecessarily.local>
2013-01-30 15:46:39 -0500

    Edits to main MATLAB control script

--- python_experiments/run_crosscat.m
+++ python_experiments/run_crosscat.m
@@ -1,9 +1,21 @@
+function run_crosscat(file_base)
+
 addpath ../matlab_code/
 
-state = initialize_from_csv('../../data/ring-data',...
-    '../../data/ring-labels', 'together');
+data_file = '../../data/' + file_base + '-data.csv';
+label_file = '../../data/' + file_base + '-labels.csv';
+
+state = initialize_from_csv(data_file,...
+    label_file, 'together');
 
 state = analyze(state, {'columnPartitionHyperparameter',...
     'columnPartitionAssignments', 'componentHyperparameters',...
     'rowPartitionHyperparameters', 'rowPartitionAssignments'},...
     100, 'all', 'all',[],[]);
+
+out_file = '../../results/' + file_base + '-cc-results.csv';
+
+save(out_file, state)
+
+
+
