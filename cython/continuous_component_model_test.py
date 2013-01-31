e6b0c9571bd064612efffbbd50aa715470b34198
dlovell <dlovell@gmail.com>
2013-01-30 23:09:15 -0500

    test get_draw

--- cython/continuous_component_model_test.py
+++ cython/continuous_component_model_test.py
@@ -5,12 +5,14 @@ hyper_map = dict()
 hyper_map["mu"] = 1.0
 hyper_map["s"] = 1.0
 hyper_map["nu"] = 1.0
-hyper_map["r"] = 1.0
+hyper_map["r"] = 2.0
 
 ccmt.set_string_double_map(hyper_map)
 component_model = ccmt.p_ContinuousComponentModel(hyper_map)
 print component_model.calc_marginal_logp()
-component_model.insert_element(1.0)
+component_model.insert_element(2.3)
 print component_model.calc_marginal_logp()
-component_model.remove_element(1.0)
+print "component_model.get_draw(0):", component_model.get_draw(0)
+print "component_model.get_draw(1):", component_model.get_draw(1)
+component_model.remove_element(2.3)
 print component_model.calc_marginal_logp()
