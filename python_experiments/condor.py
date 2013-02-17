7feecdf32927173ad9605200f3ef00f5c573f481
Peter Krafft <pkrafft@mit.edu>
2013-02-17 02:23:33 -0500

    Bug fixes to condor package

--- python_experiments/condor.py
+++ python_experiments/condor.py
@@ -23,9 +23,11 @@ License along with this package; if not, see
 http://www.gnu.org/licenses/lgpl-2.1.html
 """
 
+import pickle
 import marshal
 import types
 import os
+import sys
 
 out_dir = os.getcwd() + '/jobs/'
 if not os.path.exists(out_dir): 
@@ -38,10 +40,10 @@ def call(func, *args, **kwargs):
     
     try: 
         f = open(out_dir + 'last_id', 'r')
-        job_id = int(f.readline()) + 1
+        job_id = str(int(f.readline()) + 1)
         f.close()
-    except IOError as e:
-        job_id = 0
+    except:
+        job_id = '0'
     f = open(out_dir + 'last_id', 'w')
     f.write(job_id)
     f.close()
@@ -51,25 +53,36 @@ def call(func, *args, **kwargs):
     try:
         os.makedirs(job_dir)
     except OSError as e:
-        raise CondorPythonError('Job ' + str(job_id) + 
-                                ' already exists! Seek help.'))
+        raise CondorPythonError('Job ' + job_id + 
+                                ' already exists! Seek help.')
                 
+    write_job(job_dir, func, args)
+
+    desc_f = open(job_dir + 'description', 'w')
+
+def write_job(job_dir, func, args):
+    
     job_f = open(job_dir + 'job.py', 'w')
 
+    job_f.write('import os, sys, marshal, types, pickle\n')
+    job_f.write('os.chdir(\'' + os.getcwd() + '\')\n') 
+    job_f.write('sys.path.append(\'' + os.getcwd() + '\')\n')
+
     write_dependencies(job_f, job_dir)
-    write_func(job_f, func, args)
+    write_func(job_f, job_dir, func, args)
 
     job_f.close()
 
-    desc_f = open(job_dir + 'description', 'w')
 
 def write_func(job_f, job_dir, func, args):
     
     arg_f = open(job_dir + 'args.pkl', 'wb')
     pickle.dump(args, arg_f)
     arg_f.close()
-
-    job_f.write('args = pickle.load(' + job_dir + 'args.pkl)\n')
+    
+    job_f.write('arg_f = open(\'' + job_dir + 'args.pkl\')\n')
+    job_f.write('args = pickle.load(arg_f)\n')
+    job_f.write('arg_f.close()\n')
 
     job_f.write('def _wrapper(args):\n')
     job_f.write('    return ' + func.__name__ + '(*args)\n')
@@ -78,14 +91,18 @@ def write_func(job_f, job_dir, func, args):
 def write_dependencies(job_f, job_dir):
 
     func_num = 0
-    for name, val in globals():
+    main = __import__("__main__")
+
+    for name in dir(main):
+
+        val = getattr(main, name)
 
         if isinstance(val, types.ModuleType):
             job_f.write('import ' + val.__name__ + ' as ' + name + '\n')
 
         if isinstance(val, types.FunctionType):
             
-            f_name = job_dir + 'func_' + func_num
+            f_name = job_dir + 'func_' + str(func_num)
             func_num += 1
 
             src = marshal.dumps(val.func_code)
@@ -94,9 +111,10 @@ def write_dependencies(job_f, job_dir):
             func_f.write(src)
             func_f.close()
             
-            job_f.write('func_f = open(' + f_name + ')\n')
-            job_f.write('src = marshal.load(func_f.read())\n')
-            job_f.write(name + ' = types.FunctionType(src, globals(),' + name + ')\n')
+            job_f.write('func_f = open(\'' + f_name + '\')\n')
+            job_f.write('src = marshal.load(func_f)\n')
+            job_f.write('func_f.close()\n')
+            job_f.write(name + ' = types.FunctionType(src, globals(),\'' + name + '\')\n')
     
 class CondorPythonError(Exception):
     def __init__(self, value):
