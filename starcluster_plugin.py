4ae2464a85257c674d4aba6e96501e600e23220e
dlovell <dlovell@gmail.com>
2013-02-20 15:04:37 -0500

    fix up plugin logic

--- starcluster_plugin.py
+++ starcluster_plugin.py
@@ -6,8 +6,8 @@ from starcluster.logger import log
 #
 import tabular_predDB.settings as S
 
-run_as_user = lambda user, command_str: \
-    node.ssh.execute('sudo -u %s %s' % user, command_str)
+run_as_user = lambda node, user, command_str: \
+    node.ssh.execute('sudo -u %s %s' % (user, command_str))
 
 class tabular_predDBSetup(ClusterSetup):
      def __init__(self):
@@ -19,26 +19,34 @@ class tabular_predDBSetup(ClusterSetup):
           boto_full_file = os.path.join(S.path.remote_home_dir, '.boto')
           clone_tuple = (S.git.repo, S.path.remote_code_dir)
           checkout_tuple = (S.path.remote_code_dir, S.git.branch)
+          local_ssh_key = os.path.expanduser('~/.ssh/id_rsa')
+          remote_ssh_key = os.path.join(S.path.remote_home_dir, '.ssh')
           #
           for node in nodes:
                log.info("Copying up boto file on %s" % node.alias)
                node.ssh.put(S.s3.ec2_credentials_file, S.path.remote_home_dir)
                node.ssh.execute('chmod -R ugo+rwx %s' % boto_full_file)
-               node.ssh.put(os.path.expanduser('~/.ssh/id_rsa'), os.path.join(S.path.remote_home_dir, '.ssh'))
+               node.ssh.put(local_ssh_key, remote_ssh_key)
+               node.ssh.put(local_ssh_key, '/root/.ssh/')
           for node in nodes:
                log.info("Installing tabular_predDB (part 1) on %s" % node.alias)
                #
-               # node.ssh.execute('rm -rf %s' % clone_tuple[-1])
-               # node.ssh.execute('git clone %s %s' % clone_tuple)
+               node.ssh.execute('rm -rf %s' % clone_tuple[-1])
+               node.ssh.execute('git clone %s %s' % clone_tuple)
                node.ssh.execute('cd %s && git checkout %s' % checkout_tuple)
                node.ssh.execute('chmod -R ugo+rwx %s' % clone_tuple[-1])
+               node.ssh.execute('chown -R sgeadmin %s' % clone_tuple[-1])
           for node in nodes:
                log.info("Installing tabular_predDB (part 2) on %s" % node.alias)
                node.ssh.execute('apt-get install -y libfreetype6-dev')
                node.ssh.execute('apt-get install -y libpng12-dev')
                node.ssh.execute('pip install virtualenv')
                node.ssh.execute('pip install virtualenvwrapper')
-               command_str = 'cd %s && bash -i %s' % (S.path.remote_code_dir, S.path.virtualenv_setup_script)
-               run_as_user(user, command_str)
-               command_str = 'cd %s && bash -i %s' % (S.path.remote_code_dir, S.path.install_boost_script)
+               # install boost
+               script_full = os.path.join(S.path.remote_code_dir, S.path.install_boost_script)
+               command_str = 'bash %s >out2 2>err2' % script_full
                node.ssh.execute(command_str)
+               #
+               script_full = os.path.join(S.path.remote_code_dir, S.path.virtualenv_setup_script)
+               command_str = 'bash -i %s >out1 2>err1' % script_full
+               run_as_user(node, user, command_str)
