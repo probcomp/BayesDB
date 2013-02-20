8614e9850357dfd5718f8bd3ee9fc58e2786768a
dlovell <dlovell@gmail.com>
2013-02-20 12:56:26 -0500

    only pull down and install if gz not present (suggesting already intalled)

--- install_boost.sh
+++ install_boost.sh
@@ -5,8 +5,10 @@ with_libraries=program_options
 which_gz=${which_boost}.tar.gz
 
 cd
-wget http://downloads.sourceforge.net/project/boost/boost/1.48.0/$which_gz
-tar xvfz $which_gz
-cd $which_boost
-./bootstrap.sh --with-libraries=$with_libraries --prefix=$install_prefix
-./b2 install
+if [ ! -f $which_gz ]; then
+    wget http://downloads.sourceforge.net/project/boost/boost/1.48.0/$which_gz
+    tar xvfz $which_gz
+    cd $which_boost
+    ./bootstrap.sh --with-libraries=$with_libraries --prefix=$install_prefix
+    ./b2 install
+fi
