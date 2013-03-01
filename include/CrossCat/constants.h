66e535b7202b2040536815c8567303bfdb3f8173
dlovell <dlovell@gmail.com>
2013-03-01 12:53:27 -0500

    add variables to hold data type strings

--- include/CrossCat/constants.h
+++ include/CrossCat/constants.h
@@ -2,8 +2,12 @@
 #define GUARD_constants_h
 
 #include <limits> // MAX_INT
+#include <string>
 
 static const int MAX_INT = std::numeric_limits<int>::max();
 static const double NaN = 0./0.;
+static const std::string MULTINOMIAL_MODELTYPE = "symmetric_dirichlet_discrete";
+static const std::string CONTINUOUS_MODELTYPE = "normal_inverse_gamma";
+
 
 #endif // GUARD_constants_h
