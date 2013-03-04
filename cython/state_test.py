8e25bfccec05c1b84a76c958c81605dc98c9d3e0
dlovell <dlovell@gmail.com>
2013-03-04 18:35:18 -0500

    modify for new State.pyx:p_State definition

--- cython/state_test.py
+++ cython/state_test.py
@@ -24,23 +24,13 @@ max_mean = args.max_mean
 max_std = args.max_std
 
 # create the data
-data, inverse_permutation_indices_list = \
-    gen_data.gen_factorial_data(gen_seed, num_clusters,
-                                num_cols, num_rows, num_splits,
-                                max_mean, max_std)
-with open('data.csv', 'w') as fh:
-    data.tofile(fh, sep=',')
+T, M_r, M_c = gen_data.gen_factorial_data_objects(
+    gen_seed, num_clusters,
+    num_cols, num_rows, num_splits,
+    max_mean=max_mean, max_std=max_std,
+    )
 
-## need to actually generate clusters, this is all one cluster
-global_row_indices = range(num_rows)
-global_col_indices = range(num_cols)
-
-
-# "symmetric_dirichlet_discrete", "normal_inverse_gamma"
-
-column_types = ["normal_inverse_gamma" for global_col_idx in global_col_indices]
-event_counts = [0 for global_col_idx in global_col_indices]
-p_State = State.p_State(data, global_row_indices, global_col_indices, N_GRID=31, SEED=0)
+p_State = State.p_State(M_c, T)
 print "p_State.get_marginal_logp():", p_State.get_marginal_logp()
 for idx in range(3):
     print "transitioning"
@@ -58,7 +48,7 @@ print "X_D:", X_D
 print "X_L:", X_L
 
 constructor_args = State.transform_latent_state_to_constructor_args(X_L, X_D)
-p_State_2 = State.p_State(data, **constructor_args)
+p_State_2 = State.p_State(M_c, T, X_L, X_D) # **constructor_args)
 X_D_prime = p_State_2.get_X_D()
 X_L_prime = p_State_2.get_X_L()
 
