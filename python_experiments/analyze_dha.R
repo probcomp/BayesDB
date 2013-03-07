1a853d39752619760467aa39544570611474c488
Peter Krafft <pkrafft@mit.edu>
2013-03-07 04:42:23 -0500

    Final changes to things

--- python_experiments/analyze_dha.R
+++ python_experiments/analyze_dha.R
@@ -11,6 +11,8 @@ results = results[order(results[,3], decreasing=T),]
 
 mine = read.csv('../../mine/dha-results.csv')
 
+lm = read.csv('../../results/dha-lm.csv')
+
 z.mat = read.csv('../../crosscat-results/dha-z-results.csv',
   header=F)
 z.mat = as.matrix(z.mat + t(z.mat))
@@ -37,37 +39,42 @@ inds = (mine[,1] == compare)| (mine[,2] == compare)
 
 mine.strength = mine[inds,][order(mine[inds,1], mine[inds,2]),3]
 
-plot.mat = cbind(mine.strength,
+inds = (lm[,1] == compare)| (lm[,2] == compare)
+
+lm.sub = lm[inds,][order(lm[inds,1], lm[inds,2]),]
+lm.certainty = 1 - lm.sub[,'p']
+lm.strength = lm.sub[,'r2']
+
+plot.mat = cbind(
+  lm.certainty,
+  lm.strength,
+  mine.strength,
   z.mat[compare,-compare],
   certainty[[3]],
   strength[[3]])
 
+
 png('../../plots/dha-comparison.png')
+
+colors = brewer.pal(5, 'YlGnBu')
+pal = colorRampPalette(colors)
 
 par(mar=c(2.1,5.1,4.1,4.1))
 image(plot.mat,axes=F,col='transparent')
 axis(2,at=seq(0,1,l=ncol(plot.mat)),
      labels=c(
+       '1 - p',
+       expression(R^2),
        'MIC',
        'Z-Certainty',
        'Certainty',
        'Strength'))
-image.plot(plot.mat,add=T,legend.mar=3.1,col = pal(10))
+image.plot(plot.mat,add=T,legend.mar=3.1,col = pal(100))
 
 dev.off()
 
-par(mfrow = c(3,3))
-for(i in 1:9) {
-  col1 = mine[i,1]
-  col2 = mine[i,2]
-  plot(data[,col1], data[,col2],
-       xlab = names[col1], ylab = names[col2])
-}
-
-par(mfrow = c(3,3))
-for(i in 1:9) {
-  col1 = results[i,1]
-  col2 = results[i,2]
-  plot(data[,col1], data[,col2],
-       xlab = names[col1], ylab = names[col2])
-}
+png('../../plots/dha-example.png')
+plot(data[,9], data[,60],
+     xlab = colnames(data)[9],
+     ylab = colnames(data)[60])
+dev.off()
