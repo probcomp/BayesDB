9a77f6d4e7b53b7435ae574f27c9ebdc5608e76e
dan lovell <dlovell@gmail.com>
2013-03-14 13:55:34 -0400

    move all package installs to same line

--- install_ubuntu_packages.sh
+++ install_ubuntu_packages.sh
@@ -1,8 +1,3 @@
 pip install virtualenv
 pip install virtualenvwrapper
-apt-get install -y python-dev &
-apt-get install -y libfreetype6-dev &
-apt-get install -y tk-dev & 
-apt-get install -y libpng12-dev &
-apt-get install -y valgrind &
-wait
+apt-get install -y python-dev libfreetype6-dev tk-dev libpng12-dev valgrind
