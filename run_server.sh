dd934e4d4620252b6ce83c20e1317109b58a5de6
dan lovell <dlovell@gmail.com>
2013-03-14 14:21:52 -0400

    make server i/o unbuffered

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 workon tabular_predDB
-nohup python jsonrpc_http/server_jsonrpc.py >server_jsonrpc.out 2>server_jsonrpc.err &
+nohup python -u jsonrpc_http/server_jsonrpc.py >server_jsonrpc.out 2>server_jsonrpc.err &
