45cde6d7f4c6553cf47b6b6f9f3fed43a9048e61
dan lovell <dlovell@gmail.com>
2013-03-15 15:42:40 -0400

    create a global id to use as a default if no id is passed in

--- python_utils/api_utils.py
+++ python_utils/api_utils.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+
+global_id = 0
+
 def create_message(method_name, params, id):
     id += 1
     message = {
@@ -12,8 +15,11 @@ def create_message(method_name, params, id):
     json_message = json.dumps(message)
     return json_message, id
 
-def call(method_name, args_dict, URI, id=0, print_message=False):
+def call(method_name, args_dict, URI, id=None, print_message=False):
+    global global_id
+    if id is None: id = global_id
     message, id = create_message(method_name, args_dict, id)
+    global_id = global_id + 1
     if print_message: print 'trying message:', message
     r = requests.put(URI, data=message)
     r.raise_for_status()
