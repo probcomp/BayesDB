2a6f0a34d59283cd52c9d8c48c9f67cddc1231e1
dlovell <dlovell@gmail.com>
2013-03-16 16:45:33 -0400

    work in progress, currently simple_predictive_sample_observed doesn't match simple_predictive_sample_observed_old

--- python_utils/sample_utils.py
+++ python_utils/sample_utils.py
@@ -42,7 +42,7 @@ def simple_predictive_sample(M_c, X_L, X_D, Y, Q, get_next_seed):
             x.append(sample)
     return x
 
-def simple_predictive_sample_observed(M_c, X_L, X_D, which_row, which_column, SEED):
+def simple_predictive_sample_observed_old(M_c, X_L, X_D, which_row, which_column, SEED):
     modeltype = M_c['column_metadata'][which_column]['modeltype']
     column_hypers = X_L['column_hypers'][which_column]
     which_view = X_L['column_partition']['assignments'][which_column]
@@ -71,6 +71,17 @@ def simple_predictive_sample_observed(M_c, X_L, X_D, which_row, which_column, SE
     component_model = component_model_constructor(column_hypers,
                                                   count=cluster_count,
                                                   **component_suffstats)
+    print component_model
+    draw = component_model.get_draw(SEED)
+    return draw
+
+def simple_predictive_sample_observed(M_c, X_L, X_D, which_row, which_column, SEED):
+    which_view = X_L['column_partition']['assignments'][which_column]
+    which_cluster = X_D[which_view][which_row]
+    cluster_model = create_cluster_model_from_X_L(M_c, X_L,
+                                                  which_view, which_cluster)
+    component_model = cluster_model[which_column]
+    print component_model
     draw = component_model.get_draw(SEED)
     return draw
 
@@ -83,18 +94,34 @@ def simple_predictive_sample_observed(M_c, X_L, X_D, which_row, which_column, SE
 #   generate all data from cluster
 # join data
 
-def determine_constraints_in_view(view_state_i, Y):
-    column_names = view_state_i['column_names']
-    constraint_names = intersect(column_names, get_names(Y))
-    return list(constraint_names)
+# def get_column_reordering(M_c, column_names):
+#     name_to_idx = M_c['name_to_idx']
+#     global_column_indices = [
+#         name_to_idx[column_name]
+#         for column_name in column_names
+#         ]
+#     column_reordering = numpy.argsort(global_column_indices)
+#     return column_reordering
 
-def determine_views_involved(M_c, X_L, X_D, Y, which_columns):
-    which_views_involved = dict()
-    for which_view_idx, which_view in enumerate(X_L['view_state']):
-        constraint_names = determine_constraints_in_view(which_view, Y)
-        if len(constraint_names) != 0:
-            which_views_involved[which_view] = list(constraint_names)
-    return which_views_involved
+# def determine_constraints_in_view(view_state_i, M_c, Y):
+#     Y_subset = []
+#     name_to_idx = M_c['name_to_idx']
+#     these_column_names = set(view_state_i['column_names'])
+#     for y in Y:
+#         constraint_column = y.index
+#         constraint_column_name = name_to_idx[constraint_column]
+#         if constraint_column_name in these_column_names:
+#             Y_subset.append(y)
+#     return Y_subset
+
+# def determine_constraint_view_tuples(X_L, M_c, Y):
+#     constraint_view_tuples = []
+#     for view_state_i in X_L['view_state']:
+#         constraints_in_view = determine_constraints_in_view(
+#             view_state_i, M_c, Y)
+#         constraint_view_tuple = (constraints_in_view, view_state_i)
+#         constraint_view_tuples.append(constraint_view_tuple)
+#     return constraint_view_tuples
 
 def determine_cluster_crp_logps(view_state_i):
     counts = view_state_i['row_paritition_model']['counts']
@@ -104,33 +131,37 @@ def determine_cluster_crp_logps(view_state_i):
     logps = numpy.log(counts_appended / float(sum_counts_appended))
     return logps
 
-def get_column_reordering(M_c, column_names):
-    name_to_idx = M_c['name_to_idx']
-    global_column_indices = [
-        name_to_idx[column_name]
-        for column_name in column_names
-        ]
-    column_reordering = numpy.argsort(global_column_indices)
-    return column_reordering
-
 def extract_view_info(M_c, X_L, view_idx):
     view_state_i = X_L['view_state'][view_idx]
-    # ensure view_state_i ordering matches global ordering
     column_names = view_state_i['column_names']
-    column_reordering = get_column_reordering(M_c, column_names)
     column_component_suffstats = view_state_i['column_component_suffstats']
-    column_component_suffstats = numpy.array(column_component_suffstats)
-    column_component_suffstats = column_component_suffstats[column_reordering]
-    #
-    column_partition_assignments = X_L['column_partition']['assignments']
-    column_partition_assignments = numpy.array(column_partition_assignments)
-    is_this_view = column_partition_assignments == view_idx
-    column_metadata = numpy.array(M_c['column_metadata'])[is_this_view]
-    column_hypers = numpy.array(X_L['column_hypers'])[is_this_view]
-    row_partition_model = view_state_i['row_partition_model']
-    #
+    # ensure view_state_i ordering matches global ordering
+    global_column_indices = names_to_global_indices(column_names, M_c)
+    column_metadata = numpy.array([
+        M_c['column_metadata'][col_idx]
+        for col_idx in global_column_indices
+        ])
+    column_hypers = numpy.array([
+            X_L['column_hypers'][col_idx]
+            for col_idx in global_column_indices
+            ])
     zipped_column_info = zip(column_metadata, column_hypers, column_component_suffstats)
+    zipped_column_info = dict(zip(global_column_indices, zipped_column_info))
+    row_partition_model = view_state_i['row_partition_model']
     return zipped_column_info, row_partition_model
+
+def get_column_info_subset(zipped_column_info, column_indices):
+    column_info_subset = dict()
+    for column_index in column_indices:
+        if column_index in zipped_column_info:
+            column_info_subset[column_index] = \
+                zipped_column_info[column_index]
+    return column_info_subset
+
+def names_to_global_indices(column_names, M_c):
+    name_to_idx = M_c['name_to_idx']
+    # FIXME: str(column_name) is hack
+    return [name_to_idx[str(column_name)] for column_name in column_names]
 
 def get_component_model_constructor(modeltype):
     if modeltype == 'normal_inverse_gamma':
@@ -141,77 +172,70 @@ def get_component_model_constructor(modeltype):
         assert False, "get_model_constructor: unknown modeltype: %s" % modeltype
     return component_model_constructor
     
-def create_component_model(column_metadata, column_hypers, count=None,
-                           suffstats=None):
-    if suffstats is None: suffstats = dict()
+def create_component_model(column_metadata, column_hypers, count, suffstats):
     modeltype = column_metadata['modeltype']
     component_model_constructor = get_component_model_constructor(modeltype)
+    # FIXME: this is a hack
+    if modeltype == 'symmetric_dirichlet_discrete':
+        suffstats = dict(counts=suffstats)
     component_model = component_model_constructor(column_hypers, count,
                                                   **suffstats)
     return component_model
 
 def create_cluster_model(zipped_column_info, row_partition_model,
-                         cluster_idx=-1):
-    count = None
-    if cluster_idx != -1:
-        count = row_partition_model['counts'][cluster_idx]
-    num_cols = len(zipped_column_info)
-    cluster_component_models = []
-    for local_column_idx in range(num_cols):
+                         cluster_idx):
+    count = row_partition_model['counts'][cluster_idx]
+    cluster_component_models = dict()
+    for global_column_idx in zipped_column_info:
         column_metadata, column_hypers, column_component_suffstats = \
-            zipped_column_info[local_column_idx]
-        cluster_component_suffstats = None
-        if cluster_idx != -1:
-            cluster_component_suffstats = \
-                column_component_suffstats[cluster_idx]
+            zipped_column_info[global_column_idx]
+        cluster_component_suffstats = column_component_suffstats[cluster_idx]
         component_model = create_component_model(
             column_metadata, column_hypers, count,
             cluster_component_suffstats)
-        cluster_component_models.append(component_model)
+        cluster_component_models[global_column_idx] = component_model
     return cluster_component_models
 
-def create_cluster_models(M_c, X_L, view_idx):
+def create_empty_cluster_model(zipped_column_info):
+    cluster_component_models = dict()
+    for global_column_idx in zipped_column_info:
+        column_metadata, column_hypers, column_component_suffstats = \
+            zipped_column_info[global_column_idx]
+        component_model = create_component_model(column_metadata,
+                                                 column_hypers, None, None)
+        cluster_component_models[global_column_idx] = component_model
+    return cluster_component_models
+
+def create_cluster_models(M_c, X_L, view_idx, constraints=None):
     zipped_column_info, row_partition_model = extract_view_info(
         M_c, X_L, view_idx)
+    if constraints is not None:
+        constraint_columns = [constraint.index for constraint in constraints]
+        zipped_column_info = get_column_info_subset(
+            zipped_column_info, constraint_columns)
     cluster_models = []
     for cluster_idx in range():
         cluster_model = create_cluster_model(
             zipped_column_info, row_partition_model, cluster_idx
             )
         cluster_models.append(cluster_model)
-    cluster_model = create_cluster_model(
-        zipped_column_info, row_partition_model, -1)
-    cluster_models.append(cluster_models)
+    empty_cluster_model = create_empty_cluster_model(zipped_column_info)
+    cluster_models.append(empty_cluster_model)
     return cluster_models
-
-# FIXME: unclear how rows are passed to views
-#        this is trying to index into a row that has ALL columns
-#        which is clearly wrong
-def constraints_list_to_row(X_L, Y):
-    column_partition_assignments = X_L['column_partition']['assignments']
-    column_partition_assignments = numpy.array(column_partition_assignments)
-    is_this_view = column_partition_assignments == view_idx
-    global_col_indices = numpy.nonzero(is_this_view)[0]
-    #
-    row = [None for col_inx in global_col_indices]
-    for constraint in Y:
-        row[Y.index] = Y.value
-    return row
 
 def determine_cluster_data_logp(cluster_model, column_constraints):
     logp = 0
-    for component_model, column_constraint \
-            in zip(cluster_model, column_constraints):
-        if column_constraint is None: continue
-        logp += component_model.cacl_element_predictive_logp(column_constraint)
+    for column_constraint in column_constraints:
+        constraint_index = column_constraint.index
+        constraint_value = column_constraint.value
+        component_model = cluster_model[constraint_index]
+        logp += component_model.cacl_element_predictive_logp(constraint_value)
     return logp
 
 def determine_cluster_data_logps(M_c, X_L, X_D, Y, view_idx):
-    cluster_models = create_cluster_models(M_c, X_L, view_idx)
-    # must make column indices view local
-    column_constraints = inject_non_constraints(X_L, Y)
+    cluster_models = create_cluster_models(M_c, X_L, view_idx, constraints=Y)
     for cluster_model in cluster_models:
-        logp = determine_cluster_data_logp(cluster_model, column_constraints)
+        logp = determine_cluster_data_logp(cluster_model)
         logps.append(logp)
     return logps
 
@@ -251,34 +275,24 @@ def sample_from_view(M_c, X_L, X_D, Y, view_idx, SEED):
     sample = sample_from_cluster(cluster_model)
     return sample
 
-def simple_predictive_sample_unobserved(M_c, X_L, X_D, Y, which_column, SEED):
-    modeltype = M_c['column_metadata'][which_column]['modeltype']
-    column_hypers = X_L['column_hypers'][which_column]
-    which_view = X_L['column_partition']['assignments'][which_column]
-    view_state_i = X_L['view_state'][which_view]
-    column_names = X_L['view_state'][which_view]['column_names']
-    # find Y_prime = which conditions are in the same view as which_column
-    constraint_names = intersect(column_names, get_names(Y))
-    constraint_indices = map(lambda x: M_c['name_to_idx'][x], constraint_names)
-    # create a view comprised of columns in Y_prime
-    column_component_suffstats = \
-        X_L['view_state'][which_view]['column_component_suffstats']
-    column_component_suffstats = [
-        column_component_suffstats[x]
-        for x in constraint_indices
-        ]
-    # counts are missing from suffstats
-    which_view_ids = numpy.array(X_D[which_view])
-    counts_list = [Counter(data_col) for data_col in which_view_ids.T]
+def simple_predictive_sample_unobserved(M_c, X_L, X_D, Y, which_column,
+                                        SEED):
+    num_views = len(X_D)
+    cluster_logps_list = []
+    for view_idx in range(num_views):
+        cluster_logps = determine_cluster_logps(M_c, X_L, X_D, Y, view_idx)
+        cluster_logps_list.append(cluster_logp)
+    # sample a cluster index from each view
+    random_state = numpy.random.RandomState(SEED)
+    sample_list = []
+    for view_idx, cluster_logps in enumerate(cluster_lopgs_list):
+        draw = numpy.nonzero(numpy.random.multinomial(1, cluster_logps))[0]
+        cluster_model = create_cluster_model_from_X_L(M_c, X_L, view_idx, draw)
+        sample = sample_from_cluster(cluster_model)
+        sample_list.append(sample)
+    # FIXME: clean up and make a lookup from individual sample dictionaries
+    return sample_list
 
-    # determine logp_v for memebership in each cluster
-    # sample a cluster
-    # create a single component_model from the selected cluster and given column
-    num_clusters = len(column_component_suffstats[0])
-    for which_local_column in range(len(counts_list)):
-        for which_cluster in range(num_clusters):
-            component_suffstats = column_component_suffstats_i[which_cluster]
-            component_model = CCM.p_ContinuousComponentModel(column_hypers,
-                                                             count=cluster_count,
-                                                             **component_suffstats)
-            # do something
+def sample_from_view_cluster(M_c, X_L, X_D, Q, SEED):
+    pass
+
