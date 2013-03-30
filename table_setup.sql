9715d9526d3e3f57dfa199ed838392971326b8f3
Jay Baxter <jbaxter@mit.edu>
2013-03-30 06:07:48 +0000

    Halfway to adding integration with backend. Currently needs some minor debugging still, but now we generate T, M_r, M_c in a way that allows columns to be ignored. Also updated stub client accordingly.

--- table_setup.sql
+++ table_setup.sql
@@ -1,4 +1,4 @@
 CREATE SCHEMA preddb;
-CREATE TABLE preddb.registry(id INT PRIMARY KEY, type VARCHAR(100), url VARCHAR(200));
-CREATE TABLE preddb.table_index(tableid INT PRIMARY KEY, tablename VARCHAR(100), numsamples INT, uploadtime TIMESTAMP, analyzetime TIMESTAMP);
-CREATE TABLE preddb.models(tableid INT PRIMARY KEY, xl TEXT, latent TEXT);
+CREATE TABLE IF NOT EXISTS preddb.registry(type VARCHAR(100), url VARCHAR(200) PRIMARY KEY);
+CREATE TABLE IF NOT EXISTS preddb.table_index(tableid SERIAL PRIMARY KEY, tablename VARCHAR(100), numsamples INT, uploadtime TIMESTAMP, analyzetime TIMESTAMP, T TEXT, M_R TEXT, M_C TEXT, cctypes TEXT);
+CREATE TABLE IF NOT EXISTS preddb.models(id SERIAL PRIMARY KEY, tableid INT REFERENCES preddb.table_index(tableid), X_L TEXT, X_D TEXT, modeltime TIMESTAMP);
