190d8c092442d2d5ee9ddcdcdee7022468877c79
Jay Baxter <jbaxter@mit.edu>
2013-04-01 05:23:49 +0000

    Fix another bug in jenkins script.

--- jenkins_script.sh
+++ jenkins_script.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Remove old source, and checkout newest source from master.
 rm -rf tabular_predDB
 git clone --depth=1 https://probcomp-reserve:metropolis1953@github.com/mit-probabilistic-computing-project/tabular-predDB.git tabular_predDB
 cd tabular_predDB
+
+# If the virtualenv isn't set up, do that.
 if [ ! -e /var/lib/jenkins/.virtualenvs ]
 then
   bash -i virtualenv_setup.sh jenkins /var/lib/jenkins >virtualenv.out 2>virtualenv.err
 fi
 . /var/lib/jenkins/.virtualenvs/tabular_predDB/bin/activate
-cd /var/lib/jenkins/workspace/C++\ unit\ tests/tabular_predDB
+
+# Build and run tests. WORKSPACE is set by jenkins to /var/
+export PYTHONPATH=$WORKSPACE
+cd $WORKSPACE/tabular_predDB
 make tests
 cd tests
 nosetests --with-xunit cpp_unit_tests.py cpp_long_tests.py test_middleware.py
