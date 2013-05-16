f488329887ae4c540c1317abd978d86ec9b26be8
bigdata <bigdata@clouddev.darpa.mil>
2013-05-16 13:36:49 -0400

    bugfixes

--- binary_creation/send_hadoop_command.sh
+++ binary_creation/send_hadoop_command.sh
@@ -18,14 +18,18 @@ if [[ -z $1 ]]; then
 fi
 
 # setup
+WHICH_BINARY=$1
 HDFS_DIR="/user/bigdata/SSCI/test_remote_streaming/"
 JOBTRACKER_URI="xd-jobtracker.xdata.data-tactics-corp.com:8021"
 HDFS_URI="hdfs://xd-namenode.xdata.data-tactics-corp.com:8020/"
 WHICH_HADOOP_JAR="/usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.1.2.jar"
 
+# FIXME: should check for ${WHICH_BINARY}.jar existence on HDFS
+
 # prep local FS
 rm -rf myOutputDir
-printf "1\n2" > seed_list.txt
+> seed_list.txt
+for SEED in $(seq 0 1); do echo $SEED >> seed_list.txt; done
 # prep HDFS
 # assume jar already on HDFS
 hadoop fs -fs "$HDFS_URI" -rm -r -f "${HDFS_DIR}"myOutputDir
@@ -34,12 +38,12 @@ hadoop fs -fs "$HDFS_URI" -put seed_list.txt "${HDFS_DIR}"
 
 # run
 $HADOOP_HOME/bin/hadoop jar "$WHICH_HADOOP_JAR" \
-  -archives "${HDFS_URI}${HDFS_DIR}"${WHICH_BINARY}#${WHICH_BINARY} \
+  -archives "${HDFS_URI}${HDFS_DIR}"${WHICH_BINARY}.jar#${WHICH_BINARY} \
   -fs "$HDFS_URI" -jt "$JOBTRACKER_URI" \
   -D mapred.reduce.tasks=0 \
   -input "${HDFS_URI}${HDFS_DIR}seed_list.txt" \
   -output "${HDFS_URI}${HDFS_DIR}myOutputDir/" \
-  -mapper ${WHICH_BINARY}/${WHICH_BINARY} \
+  -mapper ${WHICH_BINARY}/hadoop_engine_helper \
   -file hadoop_input.pkl.gz \
   -cmdenv LD_LIBRARY_PATH=./${WHICH_BINARY}/:$LD_LIBRARY_PATH
 
