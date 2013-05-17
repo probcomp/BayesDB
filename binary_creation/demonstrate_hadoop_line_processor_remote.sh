91490146938afa86da5c1180c7cf6042a584283f
bigdata <bigdata@clouddev.darpa.mil>
2013-05-17 18:48:38 -0400

    correct typo in xnet_utils.py argument

--- binary_creation/demonstrate_hadoop_line_processor_remote.sh
+++ binary_creation/demonstrate_hadoop_line_processor_remote.sh
@@ -1,9 +1,9 @@
 python xnet_utils.py pickle_table_data
-python xnet_utils.py write_initialization_files --n_chains 20
+python xnet_utils.py write_initialization_files --n_chains 100
 cp initialize_input hadoop_input
 bash send_hadoop_command.sh
 cp myOutputDir/part-00000 initialize_output
-python xnet_utils.py link_initialize_to_analyze --initialize_input_filename myOutputDir/part-00000 --n_steps 20
+python xnet_utils.py link_initialize_to_analyze --initialize_output_filename myOutputDir/part-00000 --n_steps 500
 cp analyze_input hadoop_input
-bash send_hadoop_command.sh
+bash send_hadoop_command.sh 100
 cp myOutputDir/part-00000 analyze_output
