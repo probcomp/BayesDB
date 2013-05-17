76a098c8ca30283e6bf33f4240d91d4006401bac
bigdata <bigdata@clouddev.darpa.mil>
2013-05-17 10:59:26 -0400

    change name to hadoop_line_processor, don't include pylab (requires modification to State.pyx

--- binary_creation/setup.py
+++ binary_creation/setup.py
@@ -9,32 +9,33 @@ import sys
 import cx_Freeze
 
 
-excludes = []
+excludes = [
+    ]
 includes = [
     'tabular_predDB.python_utils.data_utils',
-    'tabular_predDB.python_utils.plot_utils',
     'tabular_predDB.python_utils.file_utils',
     'tabular_predDB.jsonrpc_http.Engine',
     'tabular_predDB.cython_code.State',
     'numpy',
-    'pylab',
-    'matplotlib.backends.backend_qt4agg',
+    # 'tabular_predDB.python_utils.plot_utils',
+    # 'pylab',
+    # 'matplotlib.backends.backend_qt4agg',
     ]
 
 buildOptions = dict(
         excludes = excludes,
         includes = includes,
-        compressed = True,
+        compressed = False,
 )
 
 executables = [
-        cx_Freeze.Executable("hadoop_engine_helper.py", base = None)
+        cx_Freeze.Executable("hadoop_line_processor.py", base = None)
 ]
 
 cx_Freeze.setup(
-        name = "hadoop_engine_helper",
+        name = "hadoop_line_processor",
         version = "0.1",
-        description = "initialize and transition a single markov chain",
+        description = "process arbitrary engine commands on hadoop",
         executables = executables,
         options = dict(build_exe = buildOptions))
 
