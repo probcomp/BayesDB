e474506f1ae53f754febd1d08284b7f6d0ea544e
dlovell <dlovell@gmail.com>
2013-05-20 22:01:00 -0400

    add function to create the command that runs the hadoop job

--- tabular_predDB/HadoopEngine.py
+++ tabular_predDB/HadoopEngine.py
@@ -16,34 +16,53 @@
 import os
 import inspect
 #
-import tabular_predDB.EngineTemplate as EngineTemplate
+import tabular_predDB.python_utils.general_utils as gu
+import tabular_predDB.python_utils.xnet_utils as xu
 
-
-default_which_binary = "hadoop_line_processor"
-default_hdfs_dir = "/user/bigdata/SSCI/test_remote_streaming/"
-default_jobtracker_uri = "xd-jobtracker.xdata.data-tactics-corp.com:8021"
-default_hdfs_uri = "hdfs://xd-namenode.xdata.data-tactics-corp.com:8020/"
+hadoop_home = os.environ.get('HADOOP_HOME', '')
+#
+default_which_hadoop_binary = os.path.join(hadoop_home, 'bin/hadoop')
 default_which_hadoop_jar = "/usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.1.2.jar"
+default_which_engine_binary = "hadoop_line_processor"
+default_hdfs_uri = "hdfs://xd-namenode.xdata.data-tactics-corp.com:8020/"
+default_jobtracker_uri = "xd-jobtracker.xdata.data-tactics-corp.com:8021"
+default_hdfs_dir = "/user/bigdata/SSCI/test_remote_streaming/"
+#
+input_filename = 'hadoop_input'
+table_data_filename = xu.default_table_data_filename
+output_path = 'myOutputDir'
 
 
 class HadoopEngine(object):
 
     def __init__(self, seed=0,
-                 which_binary=default_which_binary,
+                 which_engine_binary=default_which_engine_binary,
                  hdfs_dir=default_hdfs_dir,
                  jobtracker_uri=default_jobtracker_uri,
                  hdfs_uri=default_hdfs_uri,
                  which_hadoop_jar=default_which_hadoop_jar,
                  ):
-        super(HadoopEngine, self).__init__(seed)
-        self.which_binary = which_binary
-        self.hdfs_dir = self.default_hdfs_dir
-        self.jobtracker_uri = self.default_jobtracker_uri
-        self.hdfs_uri = self.default_hdfs_uri
-        self.which_hadoop_jar = self.default_which_hadoop_jar
+        # assert_vpn_is_connected()
+        #
+        self.which_hadoop_binary = default_which_hadoop_binary
+        #
+        self.seed_generator = gu.int_generator(seed)
+        self.which_engine_binary = which_engine_binary
+        self.hdfs_dir = hdfs_dir
+        self.jobtracker_uri = jobtracker_uri
+        self.hdfs_uri = hdfs_uri
+        self.which_hadoop_jar = which_hadoop_jar
         return
 
-    def initialize(self, M_c, M_r, T, initialization='from_the_prior'):
+    def initialize(self, M_c, M_r, T, initialization='from_the_prior',
+                   n_chains=1):
+        #
+        table_data = dict(M_c=M_c, M_r=M_r, T=T)
+        xu.pickle_table_data(table_data, table_data_filename)
+        xu.write_initialization_files(input_filename, n_chains)
+        put_hdfs(self.hdfs_uri, input_filename, hdfs_base_dir=self.hdfs_dir)
+        put_hdfs(self.hdfs_uri, table_data_filename,
+                 hdfs_base_dir=self.hdfs_dir)
         pass
 
     def analyze(self, M_c, T, X_L, X_D, kernel_list=(), n_steps=1, c=(), r=(),
@@ -72,6 +91,64 @@ def assert_vpn_is_connected():
     assert is_vpn_connected
     return
 
+def get_hdfs(hdfs_uri, path, hdfs_base_dir=''):
+    hdfs_path = os.path.join(hdfs_base_dir, path)
+    # clear local path
+    cmd_str = 'rm -rf %s'
+    cmd_str %= path
+    os.system(cmd_str)
+    # get from hdfs
+    cmd_str = 'hadoop fs -fs %s -get %s %s'
+    cmd_str %= (hdfs_uri, hdfs_path, path)
+    os.system(cmd_str)
+    return
+
+def put_hdfs(hdfs_uri, path, hdfs_base_dir=''):
+    hdfs_path = os.path.join(hdfs_base_dir, path)
+    # clear hdfs path
+    cmd_str = 'hadoop fs -fs %s -rm -r -f %s'
+    cmd_str %= (hdfs_uri, hdfs_path)
+    os.system(cmd_str)
+    # put to hdfs
+    cmd_str = 'hadoop fs -fs %s -put %s %s'
+    cmd_str %= (hdfs_uri, path, hdfs_path)
+    os.system(cmd_str)
+    #
+    return
+
+def create_hadoop_cmd_str(hadoop_engine, task_timeout, n_chains):
+    hdfs_path = os.path.join(hadoop_engine.hdfs_uri,
+                             hadoop_engine.hdfs_dir)
+    archive_path = os.path.join(hadoop_engine.hdfs_uri,
+                                hadoop_engine.hdfs_dir,
+                                hadoop_engine.which_engine_binary + '.jar')
+    ld_library_path = os.environ.get('LD_LIBRARY_PATH', '')
+    ld_library_path = './%s.jar:%s' % (hadoop_engine.which_engine_binary,
+                                       ld_library_path)
+    mapper_path = '%s.jar/%s' % (hadoop_engine.which_engine_binary,
+                                 hadoop_engine.which_engine_binary)
+    #
+    jar_str = '%s jar %s' % (hadoop_engine.which_hadoop_binary,
+                             hadoop_engine.which_hadoop_jar)
+    archive_str = '-archives "%s"' % archive_path
+    cmd_env_str = '-cmdenv LD_LIBRARY_PATH=%s' % ld_library_path
+    hadoop_cmd_str = ' '.join([
+            jar_str,
+            '-D mapred.task.timeout=%s' % task_timeout,
+            '-D mapred.map.tasks=%s' % n_chains,
+            archive_str,
+            '-fs "%s"' % hadoop_engine.hdfs_uri,
+            '-jt "%s"' % hadoop_engine.jobtracker_uri,
+            '-input "%s"' % os.path.join(hdfs_path, input_filename),
+            '-output "%s"' % os.path.join(hdfs_path, output_path),
+            '-mapper "%s"' % mapper_path,
+            '-reducer /bin/cat',
+            '-file %s' % input_filename,
+            '-file %s' % table_data_filename,
+            cmd_env_str,
+            ])
+    return hadoop_cmd_str
+
 def send_hadoop_command():
     pass
 
@@ -80,3 +157,18 @@ def write_hadoop_input():
 
 def read_hadoop_output():
     pass
+
+
+
+# $HADOOP_HOME/bin/hadoop jar "$WHICH_HADOOP_JAR" \
+#     -D mapred.task.timeout="${task_timeout_in_ms}" \
+#     -D mapred.map.tasks="${num_map_tasks}" \
+#     -archives "${HDFS_URI}${HDFS_DIR}${WHICH_ENGINE_BINARY}.jar" \
+#     -fs "$HDFS_URI" -jt "$JOBTRACKER_URI" \
+#     -input "${HDFS_URI}${HDFS_DIR}hadoop_input" \
+#     -output "${HDFS_URI}${HDFS_DIR}myOutputDir/" \
+#     -mapper ${WHICH_ENGINE_BINARY}.jar/${WHICH_ENGINE_BINARY} \
+#     -reducer /bin/cat \
+#     -file hadoop_input \
+#     -file table_data.pkl.gz \
+#     -cmdenv LD_LIBRARY_PATH=./${WHICH_ENGINE_BINARY}.jar/:$LD_LIBRARY_PATH
