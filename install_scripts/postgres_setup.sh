ffbc41664a5a034f9d7f2916e01b308efde8f812
dlovell <dlovell@gmail.com>
2013-05-24 21:16:31 -0400

    add jenkins user

--- install_scripts/postgres_setup.sh
+++ install_scripts/postgres_setup.sh
@@ -1,3 +1,4 @@
+sudo -u postgres createuser -s jenkins
 sudo -u postgres createuser -s sgeadmin
 sudo -u postgres createdb sgeadmin -O sgeadmin
 sudo -u sgeadmin psql -f /home/sgeadmin/tabular_predDB/install_scripts/table_setup.sql
