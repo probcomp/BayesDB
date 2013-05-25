dd1bc1fe251e4e26b06bb92f340afa5f34fb34f1
root <root@clouddev.darpa.mil>
2013-05-25 18:06:55 -0400

    parse command line arguments for user, database and script

--- install_scripts/postgres_setup.sh
+++ install_scripts/postgres_setup.sh
@@ -1,4 +1,42 @@
+#!/bin/bash
+
+
+# set default values
+user=sgeadmin
+database=sgeadmin
+script=table_setup.sql
+
+
+# print script usage
+usage() {
+    cat <<EOF
+usage: $0 options
+    
+    Set up postgres database
+    
+    OPTIONS:
+    -h      Show this message
+    -u      user=$user
+    -d      database=$database
+    -s      script=$script
+EOF
+exit
+}
+
+
+#Process the arguments
+while getopts hu:d:s: opt
+do
+    case "$opt" in
+        h) usage;;
+        u) user=$OPTARG;;
+        d) database=$OPTARG;;
+        s) script=$OPTARG;;
+    esac
+done
+
+
 sudo -u postgres createuser -s jenkins
-sudo -u postgres createuser -s sgeadmin
-sudo -u postgres createdb sgeadmin -O sgeadmin
-sudo -u sgeadmin psql -f /home/sgeadmin/tabular_predDB/install_scripts/table_setup.sql
+sudo -u postgres createuser -s $user
+sudo -u postgres createdb $database -O $user
+sudo -u $user psql -f $script
