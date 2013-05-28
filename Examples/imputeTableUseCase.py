662b036d7bb682e41350cc17c95115550ee55034
Avinash Gandhe <avinash@snoopy.(none)>
2013-05-28 12:40:27 -0400

    Modified imputeTableUseCase.py to conform to changes in CrossCat Engine api

--- Examples/imputeTableUseCase.py
+++ Examples/imputeTableUseCase.py
@@ -1,10 +1,12 @@
 # Using CrossCat to Examine Column Dependencies
 # 1. Import packages/modules needed
-import numpy, csv,  tmp_utils, pylab
+import numpy
 import tabular_predDB.python_utils.data_utils as du
 import tabular_predDB.python_utils.sample_utils as su
 import tabular_predDB.python_utils.plot_utils as pu
-from tabular_predDB.jsonrpc_http.Engine import Engine
+import CrossCatClient as ccc
+import useCase_utils as uc_utils
+
 
 # 2. Load a data table from csv file. In this example, we use synthetic data
 filename = 'flight_data_subset_missing_data.csv'
@@ -15,11 +17,14 @@ num_rows = len(T)
 num_cols = len(T[0])
 col_names = numpy.array([M_c['idx_to_name'][str(col_idx)] for col_idx in range(num_cols)])
 dataplot_filename = '{!s}_data'.format(filebase)
-tmp_utils.plot_T(T_array, M_c, filename = dataplot_filename)
+pu.plot_T(T_array, M_c, filename = dataplot_filename)
+
+for colindx in range(len(col_names)):
+    print 'Attribute: {0:30}   Model:{1}'.format(col_names[colindx],M_c['column_metadata'][colindx]['modeltype'])
 
 
 # 3. Initialize CrossCat Engine and Build Model
-engine = Engine( )
+engine = ccc.get_CrossCatClient('local', seed = 0)
 X_L_list = []
 X_D_list = []
 numChains = 10
@@ -35,8 +40,8 @@ for chain_idx in range(numChains):
 
 # 4. Impute all missing values (nan's) in the table
 numDraws = 10; # The number of draws from the distribution computed from each sample
-T_imputed = tmp_utils.impute_table(T, M_c, X_L_list, X_D_list, numDraws, engine.get_next_seed)
+T_imputed = uc_utils.impute_table(T, M_c, X_L_list, X_D_list, numDraws, engine.get_next_seed)
 
 #5. Write the new table to a csv file
 imputed_file = '{!s}_imputed.csv'.format(filebase)
-tmp_utils.write_csv(imputed_file, T_imputed, col_names.tolist())
+du.write_csv(imputed_file, T_imputed, col_names.tolist())
