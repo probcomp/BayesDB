a93a51a22c37599d4f78eaa6b9713ef138860daf
Baxter Eaves <baxyjr@gmail.com>
2013-06-03 12:20:46 -0400

    Added installation scripts

---
+++ install_scripts/mac_install/compile_cython_mac.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# compiles the cython code
+
+
+ERR_FILE=osxerrs.out
+
+# get the macports directory
+BOOST_DIR="/opt/local/include"
+
+if [ -e "$BOOST_DIR" ]
+then
+	echo "Detected default MacPorts include directory at /opt/local/include"
+else
+	if [ -z `which port` ]
+	then
+		echo "MacPorts not detected. "
+		echo "Please install MacPorts from http://www.macports.org/"
+	else
+		echo "Detected non-standard MacPorts install."
+		echo "Please enter the full path to your Boost Headers (For default install this is /opt/local/include)"
+		echo -ne "Enter:"
+
+		read BOOST_DIR
+
+		echo " "
+
+		if [ -z "$BOOST_DIR" ]
+		then
+			echo "$BOOST_DIR does not exist."
+			exit
+		fi
+	fi
+fi
+
+# source the virtualenvironment script
+source /usr/local/bin/virtualenvwrapper.sh
+if [ $? = 1 ]
+then
+	echo "failed to source virtualenvwrapper.sh."
+	echo "Compile failed. Check $ERR_FILE."
+	exit
+fi
+
+echo -ne "Compiling..."
+
+workon tabular_predDB
+
+cd cython_code
+
+if [ $? = 1 ]
+then
+	echo "workon command failed."
+	echo "Compile failed. Check $ERR_FILE."
+	exit
+fi
+
+sudo python setup.py build_ext -i -I$BOOST_DIR #1>> /dev/null 2>> $ERR_FILE
+
+# if [ $? = 1 ]
+# then
+# 	echo Failed.
+# 	echo "Compile failed. Check $ERR_FILE."
+# 	exit
+# else
+# 	echo "done."
+# 	# echo "Run Test (y/n)?"
+# 	# read run_test
+# fi
