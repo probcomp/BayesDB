e963424bfc03e969a15ebed2e1204e6a4309ed3f
dlovell <dlovell@gmail.com>
2013-06-14 20:09:06 -0400

    make clean state generation a function, move non-functions to __main__ to make script importable

--- tabular_predDB/binary_creation/runtime_scripting.py
+++ tabular_predDB/binary_creation/runtime_scripting.py
@@ -12,33 +12,11 @@ import tabular_predDB.LocalEngine as LE
 import tabular_predDB.cython_code.State as State
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--gen_seed', type=int, default=0)
-parser.add_argument('--num_clusters', type=int, default=20)
-parser.add_argument('--num_rows', type=int, default=1000)
-parser.add_argument('--num_cols', type=int, default=20)
-parser.add_argument('--num_splits', type=int, default=2)
-parser.add_argument('--max_std', type=float, default=.001)
-parser.add_argument('--n_steps', type=int, default=10)
-#
-args = parser.parse_args()
-gen_seed = args.gen_seed
-num_clusters = args.num_clusters
-num_cols = args.num_cols
-num_rows = args.num_rows
-num_splits = args.num_splits
-max_std = args.max_std
-n_steps = args.n_steps
-
-
-# helper functions
 extract_view_count = lambda X_L: len(X_L['view_state'])
-extract_ith_cluster_count = lambda view_idx, X_L: \
-    len(X_L['view_state'][0]['row_partition_model']['counts'])
-extract_0th_cluster_count = lambda X_L: extract_ith_cluster_count(0, X_L)
-get_0th_view_dims = lambda X_L: \
-    (extract_0th_cluster_count(X_L), extract_view_count(X_L))
-#
+extract_cluster_count = lambda view_state_i: view_state_i['row_partition_model']['counts']
+extract_cluster_counts = lambda X_L: map(extract_cluster_count, X_L['view_state'])
+get_state_shape = lambda X_L: (extract_view_count(X_L), extract_cluster_counts(X_L))
+
 def get_generative_clustering(M_c, M_r, T,
                               data_inverse_permutation_indices,
                               num_clusters, num_views):
@@ -72,32 +50,62 @@ def get_generative_clustering(M_c, M_r, T,
     #
     return gen_X_L, gen_X_D
 
-# generate the data
-T, M_r, M_c, data_inverse_permutation_indices = \
-    du.gen_factorial_data_objects(gen_seed, num_clusters,
-                               num_cols, num_rows, num_splits,
-                               max_mean=10, max_std=1,
-                               send_data_inverse_permutation_indices=True)
-T_array = numpy.array(T)
-# pu.plot_T(T_array, M_c, filename=None, dir='', close=False)
-X_L, X_D = get_generative_clustering(M_c, M_r, T,
-                                     data_inverse_permutation_indices,
-                                     num_clusters, num_splits)
-# pu.plot_views(T_array, X_D, X_L, M_c)
+def generate_clean_state(gen_seed, num_clusters,
+                         num_cols, num_rows, num_splits,
+                         max_mean=10, max_std=1,
+                         plot=False):
+    # generate the data
+    T, M_r, M_c, data_inverse_permutation_indices = \
+        du.gen_factorial_data_objects(gen_seed, num_clusters,
+                                   num_cols, num_rows, num_splits,
+                                   max_mean=10, max_std=1,
+                                   send_data_inverse_permutation_indices=True)
+    # recover generative clustering
+    X_L, X_D = get_generative_clustering(M_c, M_r, T,
+                                         data_inverse_permutation_indices,
+                                         num_clusters, num_splits)
+    if plot:
+        T_array = numpy.array(T)
+        pu.plot_views(T_array, X_D, X_L, M_c)
+    return T, M_c, M_r, X_L, X_D
 
-# run some transitions
-all_kernels = State.transition_name_to_method_name_and_args.keys()
-local_engine = LE.LocalEngine()
-for which_kernel in all_kernels:
-    # set alphas to something unlikely to split out elements
-    X_L['view_state'][0]['row_partition_model']['hypers']['alpha'] = 0.01
-    X_L['column_partition']['hypers']['alpha'] = 0.01
-    kernel_list = (which_kernel,)
-    start_dims = get_0th_view_dims(X_L)
-    timer_message = 'n_steps=%s of %s kernel' % (n_steps, which_kernel)
-    with gu.Timer(timer_message) as timer:
-        X_L, X_D = local_engine.analyze(M_c, T, X_L, X_D, n_steps=n_steps,
-                                        kernel_list=kernel_list)
-    end_dims = get_0th_view_dims(X_L)
-    print 'start_dims, end_dims: %s, %s' % (start_dims, end_dims)
-    # pu.plot_views(T_array, X_D, X_L, M_c)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gen_seed', type=int, default=0)
+    parser.add_argument('--num_clusters', type=int, default=20)
+    parser.add_argument('--num_rows', type=int, default=1000)
+    parser.add_argument('--num_cols', type=int, default=20)
+    parser.add_argument('--num_splits', type=int, default=2)
+    parser.add_argument('--max_std', type=float, default=.001)
+    parser.add_argument('--n_steps', type=int, default=10)
+    #
+    args = parser.parse_args()
+    gen_seed = args.gen_seed
+    num_clusters = args.num_clusters
+    num_cols = args.num_cols
+    num_rows = args.num_rows
+    num_splits = args.num_splits
+    max_std = args.max_std
+    n_steps = args.n_steps
+
+    T, M_c, M_r, X_L, X_D = generate_clean_state(gen_seed, num_clusters,
+                                                 num_cols, num_rows, num_splits,
+                                                 max_mean=10, max_std=1)
+
+    # run some transitions
+    all_kernels = State.transition_name_to_method_name_and_args.keys()
+    local_engine = LE.LocalEngine()
+    for which_kernel in all_kernels:
+        # set alphas to something unlikely to split out elements
+        X_L['view_state'][0]['row_partition_model']['hypers']['alpha'] = 0.01
+        X_L['column_partition']['hypers']['alpha'] = 0.01
+        kernel_list = (which_kernel,)
+        start_dims = get_state_shape(X_L)
+        timer_message = 'n_steps=%s of %s kernel' % (n_steps, which_kernel)
+        with gu.Timer(timer_message) as timer:
+            X_L, X_D = local_engine.analyze(M_c, T, X_L, X_D, n_steps=n_steps,
+                                            kernel_list=kernel_list)
+        end_dims = get_state_shape(X_L)
+        print 'start_dims, end_dims: %s, %s' % (start_dims, end_dims)
+        # pu.plot_views(T_array, X_D, X_L, M_c)
