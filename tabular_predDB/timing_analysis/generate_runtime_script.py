7aef72b59b70adee3b3fb7b523cb7647f728cd80
dlovell <dlovell@gmail.com>
2013-07-07 14:52:30 -0400

    change order of variation of parameters

--- tabular_predDB/timing_analysis/generate_runtime_script.py
+++ tabular_predDB/timing_analysis/generate_runtime_script.py
@@ -5,20 +5,20 @@ n_steps = 10
 #
 base_str = ' '.join([
   'python runtime_scripting.py',
-  '--num_clusters %s',
   '--num_rows %s',
   '--num_cols %s',
+  '--num_clusters %s',
   '--num_splits %s',
   '--n_steps %s' % n_steps,
   '-do_remote >>out 2>>err &',
   ])
 
-num_clusters_list = [5, 10, 20, 25, 40]
 num_rows_list = [100, 400, 1000]
-num_cols_list = [4, 10, 20, 40]
+num_cols_list = [4, 16, 32]
+num_clusters_list = [10, 20, 50]
 num_splits_list = [1, 2, 4]
-take_product_of = [num_clusters_list, num_rows_list, num_cols_list, num_splits_list]
-for num_clusters, num_rows, num_cols, num_splits \
+take_product_of = [num_rows_list, num_cols_list, num_clusters_list, num_splits_list]
+for num_rows, num_cols, num_clusters, num_splits \
     in itertools.product(*take_product_of):
-  this_base_str = base_str % (num_clusters, num_rows, num_cols, num_splits)
+  this_base_str = base_str % (num_rows, num_cols, num_clusters, num_splits)
   print this_base_str
