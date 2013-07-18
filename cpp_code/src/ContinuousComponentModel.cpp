4820862c378748537e77f733475c213ed876b22f
Baxter Eaves <baxyjr@gmail.com>
2013-07-18 11:06:38 -0400

    cleanup

--- cpp_code/src/ContinuousComponentModel.cpp
+++ cpp_code/src/ContinuousComponentModel.cpp
@@ -13,7 +13,7 @@
 *   See the License for the specific language governing permissions and
 *   limitations under the License.
 */
-
+#include "ContinuousComponentModel.h"
 using namespace std;
 
 ContinuousComponentModel::ContinuousComponentModel(map<string, double> &in_hypers) {
