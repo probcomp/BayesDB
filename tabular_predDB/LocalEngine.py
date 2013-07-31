9304db480d83d25ccfb7fd280a7ce4818a14e839
dlovell <dlovell@gmail.com>
2013-07-31 16:09:43 -0400

    document all class methods

--- tabular_predDB/LocalEngine.py
+++ tabular_predDB/LocalEngine.py
@@ -19,11 +19,28 @@ import tabular_predDB.python_utils.sample_utils as su
 
 
 class LocalEngine(EngineTemplate.EngineTemplate):
+    """A simple interface to the Cython-wrapped C++ engine
+
+    LocalEngine holds no state other than a seed generator.
+
+    """
 
     def __init__(self, seed=0):
         super(LocalEngine, self).__init__(seed=seed)
 
     def initialize(self, M_c, M_r, T, initialization='from_the_prior'):
+        """Sample a latent state from prior
+
+        :param M_c: The column metadata
+        :type M_c: dict
+        :param M_r: The row metadata
+        :type M_r: dict
+        :param T: The data table in mapped representation (all floats)
+        :type T: list of lists
+        :returns: X_L, X_D -- the latent state
+
+        """
+
         # FIXME: why is M_r passed?
         SEED = self.get_next_seed()
         X_L, X_D = _do_initialize(M_c, M_r, T, initialization, SEED)
@@ -31,6 +48,34 @@ class LocalEngine(EngineTemplate.EngineTemplate):
 
     def analyze(self, M_c, T, X_L, X_D, kernel_list=(), n_steps=1, c=(), r=(),
                 max_iterations=-1, max_time=-1):
+        """Evolve the latent state by running transition kernels
+
+        :param M_c: The column metadata
+        :type M_c: dict
+        :param T: The data table in mapped representation (all floats)
+        :type T: list of lists
+        :param X_L: the latent variables associated with the latent state
+        :type X_L: dict
+        :param X_D: the particular cluster assignments of each row in each view
+        :type X_D: list of lists
+        :param kernel_list: names of the transition kernels to run
+        :type kernel_list: list of strings
+        :param n_steps: the number of times to run each transition kernel
+        :type n_steps: int
+        :param c: the (global) column indices to run transitions on
+        :type c: list of ints
+        :param r: the (global) row indices to run transitions on
+        :type r: list of ints
+        :param max_iterations: the maximum number of times ot run each transition
+                               kernel. Applicable only if max_time != -1.
+        :type max_iterations: int
+        :param max_time: the maximum amount of time (seconds) to run transitions
+                         for before stopping to return progress
+        :type max_time: float
+        :returns: X_L, X_D -- the evolved latent state
+
+        """
+
         SEED = self.get_next_seed()
         X_L_prime, X_D_prime = _do_analyze(M_c, T, X_L, X_D,
                                            kernel_list, n_steps, c, r,
@@ -39,18 +84,103 @@ class LocalEngine(EngineTemplate.EngineTemplate):
         return X_L_prime, X_D_prime
 
     def simple_predictive_sample(self, M_c, X_L, X_D, Y, Q, n=1):
+        """Sample values from the predictive distribution of the given latent state
+
+        :param M_c: The column metadata
+        :type M_c: dict
+        :param X_L: the latent variables associated with the latent state
+        :type X_L: dict
+        :param X_D: the particular cluster assignments of each row in each view
+        :type X_D: list of lists
+        :param Y: A list of constraints to apply when sampling.  Each constraint
+                  is a triplet of (r, d, v): r is the row index, d is the column
+                  index and v is the value of the constraint
+        :type Y: list of lists
+        :param Q: A list of values to sample.  Each value is doublet of (r, d):
+                  r is the row index, di is the column index
+        :type Q: list of lists
+        :param n: the number of samples to draw
+        :type n: int
+        :returns: list of floats -- samples in the same order specified by Q
+
+        """
         get_next_seed = self.get_next_seed
         samples = _do_simple_predictive_sample(M_c, X_L, X_D, Y, Q, n, get_next_seed)
         return samples
 
     def simple_predictive_probability(self, M_c, X_L, X_D, Y, Q, epsilon=0.001):
+        """Calculate the probability of a cell taking a value within epsilon of 
+        the specified values given a latent state
+
+        :param M_c: The column metadata
+        :type M_c: dict
+        :param X_L: the latent variables associated with the latent state
+        :type X_L: dict
+        :param X_D: the particular cluster assignments of each row in each view
+        :type X_D: list of lists
+        :param Y: A list of constraints to apply when sampling.  Each constraint
+                  is a triplet of (r, d, v): r is the row index, d is the column
+                  index and v is the value of the constraint
+        :type Y: list of lists
+        :param Q: A list of values to sample.  Each value is doublet of (r, d):
+                  r is the row index, di is the column index
+        :type Q: list of lists
+        :param epsilon: the window around the specified value to take the delta
+                        in cdf of
+        :type epsilon: float
+        :returns: list of floats -- probabilities of the values specified by Q
+
+        """
         return su.simple_predictive_probability(M_c, X_L, X_D, Y, Q, epsilon)
 
     def impute(self, M_c, X_L, X_D, Y, Q, n):
+        """Impute values from the predictive distribution of the given latent state
+
+        :param M_c: The column metadata
+        :type M_c: dict
+        :param X_L: the latent variables associated with the latent state
+        :type X_L: dict
+        :param X_D: the particular cluster assignments of each row in each view
+        :type X_D: list of lists
+        :param Y: A list of constraints to apply when sampling.  Each constraint
+                  is a triplet of (r, d, v): r is the row index, d is the column
+                  index and v is the value of the constraint
+        :type Y: list of lists
+        :param Q: A list of values to sample.  Each value is doublet of (r, d):
+                  r is the row index, di is the column index
+        :type Q: list of lists
+        :param n: the number of samples to use in the imputation
+        :type n: int
+        :returns: list of floats -- imputed values in the same order as
+                  specified by Q
+
+        """
         e = su.impute(M_c, X_L, X_D, Y, Q, n, self.get_next_seed)
         return e
 
     def impute_and_confidence(self, M_c, X_L, X_D, Y, Q, n):
+        """Impute values and confidence of the value from the predictive
+        distribution of the given latent state
+
+        :param M_c: The column metadata
+        :type M_c: dict
+        :param X_L: the latent variables associated with the latent state
+        :type X_L: dict
+        :param X_D: the particular cluster assignments of each row in each view
+        :type X_D: list of lists
+        :param Y: A list of constraints to apply when sampling.  Each constraint
+                  is a triplet of (r, d, v): r is the row index, d is the column
+                  index and v is the value of the constraint
+        :type Y: list of lists
+        :param Q: A list of values to sample.  Each value is doublet of (r, d):
+                  r is the row index, di is the column index
+        :type Q: list of lists
+        :param n: the number of samples to use in the imputation
+        :type n: int
+        :returns: list of lists -- list of (value, confidence) tuples in the
+                  same order as specified by Q
+
+        """
         if isinstance(X_L, (list, tuple)):
             assert isinstance(X_D, (list, tuple))
             # TODO: multistate impute doesn't exist yet
