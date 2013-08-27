2ffecfccbcfa9dc173edff31e705c0a16180ca78
dlovell <dlovell@gmail.com>
2013-08-26 22:19:15 -0400

    BUGFIX: make __main__'s HEADER print to stdout format the same as parse_timing_to_csv print to file

--- tabular_predDB/timing_analysis/parse_timing.py
+++ tabular_predDB/timing_analysis/parse_timing.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
         for line in fh:
             lines.append(xu.parse_hadoop_line(line))
 
-    header = 'num_rows, num_cols, num_clusters, num_views, time_per_step, which_kernel'
+    header = 'num_rows,num_cols,num_clusters,num_views,time_per_step,which_kernel'
     print header
     reduced_lines = map(lambda x: x[1], lines)
     for reduced_line in reduced_lines:
