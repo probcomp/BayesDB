1d32007ded345d6ddbebc0aa9518dbd4ac0f1863
Jay Baxter <jbaxter@mit.edu>
2013-10-30 22:38:49 +0000

    update bayesdb server script

--- scripts/run_bayesdb_server.sh
+++ scripts/run_bayesdb_server.sh
@@ -1,4 +1,3 @@
-#workon tabular_predDB
-pkill -f python\.\*/bayesdb_server_jsonrpc.py
+pkill -f python\.\*/jsonrpc_server.py
 mkdir -p server_logs
-nohup python -u bayesdb/jsonrpc/bayesdb_server_jsonrpc.py >server_logs/bayesdb_server_jsonrpc.out 2>server_logs/bayesdb_server_jsonrpc.err &
+nohup python -u bayesdb/jsonrpc_server.py >server_logs/jsonrpc_server.out 2>server_logs/jsonrpc_server.err &
