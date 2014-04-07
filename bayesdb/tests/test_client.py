340ac537950c5bd1c53ba52fccf33731aa870300
benmayne <benmayne-git@mit.edu>
2014-04-07 10:40:33 -0400

    fixed again

--- bayesdb/tests/test_client.py
+++ bayesdb/tests/test_client.py
@@ -380,4 +380,4 @@ def test_select_where_col_equal_val():
   client('initialize 2 models for %s' % (test_tablename), debug=True, pretty=False)
   basic_similarity = client('select * from %s where similarity to 1 > .6 limit 5' % (test_tablename),pretty=False, debug=True)[0]['row_id']
   col_val_similarity = client('select * from %s where similarity to name = "Akron OH" > .6 limit 5' % (test_tablename),pretty=False, debug=True)[0]['row_id']
-  assert all(basic_similarity & col_val_similarity)
+  assert len(basic_similarity) == len(col_val_similarity)
