17f4b493a46f3ac5dc1119e077092a22790efc7e
benmayne <benmayne-git@mit.edu>
2014-04-07 10:23:00 -0400

    fixed test bug

--- bayesdb/tests/test_client.py
+++ bayesdb/tests/test_client.py
@@ -375,6 +375,9 @@ def test_pandas():
   client("create btable %s from pandas" % (test_tablename), debug=True, pretty=False, pandas_df=test_df)
 
 def test_select_where_col_equal_val():
-  basic_similarity = client('select * from dha where similarity to 1 > .6 limit 5',pretty=False, debug=True)[0]['row_id']
-  col_val_similarity = client('select * from dha where similarity to name = "Akron OH" > .6 limit 5',pretty=False, debug=True)[0]['row_id']
+  test_tablename = create_dha()
+  global client, test_filenames
+  client('initialize 2 models for %s' % (test_tablename), debug=True, pretty=False)
+  basic_similarity = client('select * from %s where similarity to 1 > .6 limit 5' % (test_tablename),pretty=False, debug=True)[0]['row_id']
+  col_val_similarity = client('select * from %s where similarity to name = "Akron OH" > .6 limit 5' % (test_tablename),pretty=False, debug=True)[0]['row_id']
   assert all(basic_similarity & col_val_similarity)
