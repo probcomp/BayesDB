0a6daae98129df29884667cfd2fefbc6317ef143
Mason DeCamillis <md2477@gmail.com>
2014-08-14 08:36:46 -0400

    Numeric output is now truncated at two decimal places and right-aligned. Eventually will need to revisit this because we may want more decimal places for columns with small values.

--- bayesdb/client.py
+++ bayesdb/client.py
@@ -353,10 +353,22 @@ class Client(object):
         if 'data' in query_obj and 'columns' in query_obj:
             """ Pretty-print data table """
             pt = prettytable.PrettyTable()
-            pt.field_names = query_obj['columns']
-            for row in query_obj['data']:
-                pt.add_row(row)
+            columns = query_obj['columns']
+            pt.field_names = columns
+
+            # Adjust value width - for now preserve 2 decimal places.
+            for row_idx, row_values in enumerate(query_obj['data']):
+                if type(row_values) == tuple:
+                    row_values = list(row_values)
+                for col_idx, col_value in enumerate(row_values):
+                    if type(col_value) == float:
+                        # Right-align numeric columns.
+                        if row_idx == 0:
+                            pt.align[columns[col_idx]] = 'r'
+                        row_values[col_idx] = "% .2f" % col_value
+                pt.add_row(row_values)
             result += str(pt)
+
         elif 'list' in query_obj:
             """ Pretty-print lists """
             result += str(query_obj['list'])
